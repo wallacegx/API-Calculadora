17dee86bc5a84e42a749d5316f0991af593806fe
noctisgx <63266576+wallacegx@users.noreply.github.com>
2020-09-16 21:43:57 -0300

    v3

--- Main.py
+++ Main.py
@@ -1,29 +1,32 @@
-from flask_ngrok import run_with_ngrok
-from flask import Flask, request, jsonify
+import os
+from flask import Flask, jsonify
 from flask_cors import CORS
 from math import sqrt
 
 app = Flask(__name__)
 
-cors = CORS(app, resource={r"/*":{"origins": "*"}})
+cors = CORS(app, resource={r"/*": {"origins": "*"}})
 
-#-----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+
 
 @app.route('/')
 def root():
     return 'Digite qual operação gostaria de fazer entre as opções'
 
-#-----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+
 
 @app.route('/sum?value1=<value1>&value2=<value2>', methods=['GET'])
-def sum(value1,value2):
+def somar(value1, value2):
     try:
-        valor1 = int(value1);
+        valor1 = int(value1)
     except:
         return 'Primeiro valor inválido.'
 
     try:
-        valor2 = int(value2);
+        valor2 = int(value2)
     except:
         return 'Segundo valor inválido.'
 
@@ -31,17 +34,19 @@ def sum(value1,value2):
 
     return jsonify(ret)
 
-#-----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+
 
 @app.route('/subtraction?value1=<value1>&value2=<value2>', methods=['GET'])
 def subtraction(value1,value2):
     try:
-        valor1 = int(value1);
+        valor1 = int(value1)
     except:
         return 'Primeiro valor inválido.'
 
     try:
-        valor2 = int(value2);
+        valor2 = int(value2)
     except:
         return 'Segundo valor inválido.'
 
@@ -49,17 +54,19 @@ def subtraction(value1,value2):
 
     return jsonify(ret)
 
-#-----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+
 
 @app.route('/division?value1=<value1>&value2=<value2>', methods=['GET'])
 def division(value1,value2):
     try:
-        valor1 = int(value1);
+        valor1 = int(value1)
     except:
         return 'Primeiro valor inválido.'
 
     try:
-        valor2 = int(value2);
+        valor2 = int(value2)
     except:
         return 'Segundo valor inválido.'
 
@@ -67,17 +74,19 @@ def division(value1,value2):
 
     return jsonify(ret)
 
-#-----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+
 
 @app.route('/multiplication?value1=<value1>&value2=<value2>', methods=['GET'])
 def multiplication(value1,value2):
     try:
-        valor1 = int(value1);
+        valor1 = int(value1)
     except:
         return 'Primeiro valor inválido.'
 
     try:
-        valor2 = int(value2);
+        valor2 = int(value2)
     except:
         return 'Segundo valor inválido.'
 
@@ -85,12 +94,14 @@ def multiplication(value1,value2):
 
     return jsonify(ret)
 
-#-----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+
 
 @app.route('/squareroot?value=<value>', methods=['GET'])
 def squareroot(value):
     try:
-        valor1 = int(value);
+        valor1 = int(value)
     except:
         return 'Valor inválido.'
 
@@ -98,17 +109,19 @@ def squareroot(value):
 
     return jsonify(ret)
 
-#-----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+
 
 @app.route('/power?base=<base>&exponent=<exponent>', methods=['GET'])
 def power(base,exponent):
     try:
-        li_base = int(base);
+        li_base = int(base)
     except:
         return 'Base Inválida.'
 
     try:
-        li_exponent = int(exponent);
+        li_exponent = int(exponent)
     except:
         return 'Expoente inválido.'
 
@@ -116,22 +129,24 @@ def power(base,exponent):
 
     return jsonify(ret)
 
-#-----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+
 
 @app.route('/arithmeticaverage?value1=<value1>&value2=<value2>&value3=<value3>', methods=['GET'])
 def arithmeticaverage(value1,value2,value3):
     try:
-        valor1 = int(value1);
+        valor1 = int(value1)
     except:
         return 'Primeiro valor inválido.'
 
     try:
-        valor2 = int(value2);
+        valor2 = int(value2)
     except:
         return 'Segundo valor inválido.'
 
     try:
-        valor3 = int(value3);
+        valor3 = int(value3)
     except:
         return 'Terceiro valor inválido.'
 
@@ -139,22 +154,24 @@ def arithmeticaverage(value1,value2,value3):
 
     return jsonify(ret)
 
-#-----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+
 
 @app.route('/harmonicmean?value1=<value1>&value2=<value2>&value3=<value3>', methods=['GET'])
 def harmonicmean(value1,value2,value3):
     try:
-        valor1 = int(value1);
+        valor1 = int(value1)
     except:
         return 'Primeiro valor inválido.'
 
     try:
-        valor2 = int(value2);
+        valor2 = int(value2)
     except:
         return 'Segundo valor inválido.'
 
     try:
-        valor3 = int(value3);
+        valor3 = int(value3)
     except:
         return 'Terceiro valor inválido.'
 
@@ -162,22 +179,24 @@ def harmonicmean(value1,value2,value3):
 
     return jsonify(ret)
 
-#-----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+
 
 @app.route('/mod?value1=<value1>&value2=<value2>&value3=<value3>', methods=['GET'])
 def mod(value1,value2,value3):
     try:
-        valor1 = int(value1);
+        valor1 = int(value1)
     except:
         return 'Primeiro valor inválido.'
 
     try:
-        valor2 = int(value2);
+        valor2 = int(value2)
     except:
         return 'Segundo valor inválido.'
 
     try:
-        valor3 = int(value3);
+        valor3 = int(value3)
     except:
         return 'Terceiro valor inválido.'
 
